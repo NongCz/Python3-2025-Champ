d9cae75667bbc87ba946da994d7c483123c0886c
NongCz <Boonlerd2400@gmail.com>
2025-01-14 10:38:18 +0700

    Homework2

--- Day2/Fly Circle2.py
+++ Day2/Fly Circle2.py
@@ -11,43 +11,33 @@ class Circle:
         self.screen_width = screen_width
         self.screen_height = screen_height
 
-    def draw(self, screen):
-        pygame.draw.circle(screen, (0, 0, 255), (self.x, self.y), self.radius)
-
-    def move(self):
+    def move_and_draw(self, screen):
         self.x += self.x_velocity
         self.y += self.y_velocity
-
         if self.x - self.radius <= 0 or self.x + self.radius >= self.screen_width:
             self.x_velocity *= -1
         if self.y - self.radius <= 0 or self.y + self.radius >= self.screen_height:
             self.y_velocity *= -1
+        pygame.draw.circle(screen, (0, 0, 255), (self.x, self.y), self.radius)
 
 pygame.init()
-screen_width, screen_height = 800, 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Bouncing Circle")
+screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
-circle1 = Circle(radius=30, x=200, y=300, x_velocity=4, y_velocity=3, screen_width=screen_width, screen_height=screen_height)
-circle2 = Circle(radius=40, x=500, y=400, x_velocity=-3, y_velocity=5, screen_width=screen_width, screen_height=screen_height)
+circles = [
+    Circle(30, 200, 300, 4, 3, 800, 600),
+    Circle(40, 500, 400, -3, 5, 800, 600),
+    Circle(20, 100, 200, -2, 7, 800, 600)
+]
 
-running = True
-while running:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
 
     screen.fill((255, 255, 255))
-
-    circle1.move()
-    circle1.draw(screen)
-    circle2.move()
-    circle2.draw(screen)
-
+    for circle in circles:
+        circle.move_and_draw(screen)
     pygame.display.flip()
-
     clock.tick(60)
-
-pygame.quit()
-sys.exit()
